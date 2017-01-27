1986fe0b6fb1943d7ef727c9b9e5a28d2ee9258c
Jeff Wolski <wolski@uber.com>
2017-01-27 15:54:28 +0100

    Added log lines to server Put

--- server/routes.go
+++ server/routes.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/jwolski/logstore/api"
 
 	"golang.org/x/net/context"
@@ -30,9 +32,14 @@ func (r *routes) Put(ctx context.Context, req *api.PutRequest) (*api.PutResponse
 
 	// Save log record to the storage backend. Respond with a `errSave` if
 	// anything goes wrong.
-	if err := r.store.save(req2LogRecord(req)); err != nil {
+	rec := req2LogRecord(req)
+	if err := r.store.save(rec); err != nil {
 		resp.ErrCode = errSave
+		log.Printf("Failed to store log record with request ID: %s", rec.RequestId)
+		return resp, nil
 	}
-
+	// TODO: We probably don't need to log requests to this degree. But it is
+	// nice debug information...
+	log.Printf("Stored log record with request ID: %s", rec.RequestId)
 	return resp, nil
 }
